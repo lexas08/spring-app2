a606a1226dd399917d26a9cb4eab0b1c375b64b2
lexas08 <alexkuk1@yandex.ru>
2019-10-19 20:39:03 +0300

    SECOND LESSON

---
+++ src/main/java/ru/kukatov/springcourse/Music.java
@@ -0,0 +1,5 @@
+package ru.kukatov.springcourse;
+
+public interface Music {
+    String getSong();
+}
---
+++ src/main/java/ru/kukatov/springcourse/RockMusic.java
@@ -0,0 +1,8 @@
+package ru.kukatov.springcourse;
+
+public class RockMusic implements Music {
+    @Override
+    public String getSong() {
+        return "Wind cries Mary";
+    }
+}
